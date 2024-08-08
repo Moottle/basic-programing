012b64839086652cd0799152750bf82f6036a6d8
Moottle <moottle2548@gmail.com>
2024-08-08 16:16:29 +0700

    ส่งงาน ep 1

---
+++ ep1/index.ts
@@ -0,0 +1,18 @@
+const firstname: string = 'Siravit'
+
+let Y: number = 10
+Y = 20
+
+let iscomehome: boolean = false
+
+console.log (firstname)
+console.log (Y)
+console.log (iscomehome)
+
+const a: number = 1
+const b: number = 2
+
+console.log (a == b)
+
+console.log (a > b)
+console.log (a <= b)
